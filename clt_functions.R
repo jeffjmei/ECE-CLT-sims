0e14b9a454d8aca0fb9b40bfeafdfd2444c35096
jeffjmei <jeffreymei926@gmail.com>
2025-02-09 11:15:27 -0700

    added test_bound function
    used to test whether or not a bound is satisfied

--- clt_functions.R
+++ clt_functions.R
@@ -1,5 +1,5 @@
 # Functions
-mod_idx <- function(i, n=1000) {
+mod_idx <- function(i, n = 1000) {
   (i - 1) %% n + 1
 }
 
@@ -11,16 +11,24 @@ conf_int <- function(X) {
   )
 }
 
-Tk <- function(X, Y=NULL, k=1){
+Tk <- function(X, Y = NULL, k = 1) {
   n <- length(X)
 
-  if(is.null(Y)){
-    sum((X[mod_idx(1:n, n)] - X[mod_idx(1:n-k, n)])^2)
-  }
-  else{
+  if (is.null(Y)) {
+    sum((X[mod_idx(1:n, n)] - X[mod_idx(1:n - k, n)])^2)
+  } else {
     sum(
-      (X[mod_idx(1:n, n)] - X[mod_idx(1:n-k, n)]) * 
-      (Y[mod_idx(1:n, n)] - Y[mod_idx(1:n-k, n)])
+      (X[mod_idx(1:n, n)] - X[mod_idx(1:n - k, n)]) *
+        (Y[mod_idx(1:n, n)] - Y[mod_idx(1:n - k, n)])
     )
   }
+}
+
+test_bound <- function(test_name, true_value, sim_value) {
+  ci <- conf_int(sim_value)
+  test_that(test_name, {
+    expect_true(
+      (ci[1] < true_value) & (true_value < ci[2])
+    )
+  })
 }
